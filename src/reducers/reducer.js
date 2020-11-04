2020ed13662ac5bc2b68a0dad0a505fe6da21a82
reesharper <reestimharper@gmail.com>
2020-11-03 22:55:57 -0500

    added dependencies and updated readMe

---
+++ src/reducers/reducer.js
@@ -0,0 +1,16 @@
+
+
+const initialState = {
+
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+
+    default:
+    return state;
+  }
+};
+
+const reducerExport = { initialState, reducer }
+export default reducerExport
