82d1952dcf660ea283d960e06261216fc14f5ee7
reesharper <reestimharper@gmail.com>
2020-12-09 03:33:57 -0500

    finished add feature

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import {addFeature} from '../actions/actions';
+import { connect } from 'react-redux'
 
 const AdditionalFeature = props => {
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    props.addFeature(props.feature)
+    console.log(props.feature)
+    console.log(props.car.features)
+  }
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={handleClick} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+const mapStateToProps = state => {
+  return {
+    car: state.car,
+    additionalFeatures: state.additionalFeatures,
+    additionalPrice: state.additionalPrice,
+  };
+};
+
+
+export default connect(mapStateToProps, { addFeature })(AdditionalFeature);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducer } from './reducers/reducer'
+
+
+const store = createStore(reducer)
+// console.log(store.getState())
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+, rootElement);
